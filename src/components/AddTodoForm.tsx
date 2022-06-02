d6de0478db71bb603a4675d64c2e053c55b6cd59
Joseph Schartung <104148306+JosephSchartung@users.noreply.github.com>
2022-06-02 14:40:44 -0700

    fixed button appearance for adding task

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -24,7 +24,7 @@ export const AddTodoForm: React.FC<Props> = ({ addTodo }) => {
         <div className="AddTodoForm">
             <form>
                 <input className="white-text" type="text" value={desc} onChange={handleChange}/>
-                <button className="btn btn-small waves-effect waves-light hoverable transparent" onClick={handleSubmit}>Add Task</button>
+                <button style={{borderRadius:"20px"}}className="btn btn-small waves-effect waves-light hoverable transparent" onClick={handleSubmit}>Add Task</button>
             </form>
         </div>
 
